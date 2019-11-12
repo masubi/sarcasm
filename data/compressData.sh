2b3debea77ed68d5e773277be56b168a4d0d337f
Justin Masui <jumasui@cisco.com>
2019-11-11 16:31:46 -0800

    automated commit

--- data/compressData.sh
+++ data/compressData.sh
@@ -3,7 +3,9 @@
 tar -czf train-balanced-sarcasm.gz train-balanced-sarcasm.csv
 split -b 45mb train-balanced-sarcasm.gz "train-balanced-sarcasm.part"
 rm train-balanced-sarcasm.gz
+rm train-balanced-sarcasm.csv
 
 tar -czf sarcasm_kaggle.gz sarcasm_kaggle.csv
 split -b 45mb sarcasm_kaggle.gz "sarcasm_kaggle.part"
 rm sarcasm_kaggle.gz
+rm sarcasm_kaggle.csv
