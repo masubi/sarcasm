a2ce05ef3596cf1ea09e340ea08847ad152fc243
Justin Masui <jumasui@cisco.com>
2019-11-13 11:33:27 -0800

    automated commit

--- data/extractData.sh
+++ data/extractData.sh
@@ -1,12 +1,12 @@
 #/bin/bash
 
-#cat train-balanced-sarcasm.parta* > train-balanced-sarcasm.gz
-#tar -xvf train-balanced-sarcasm.gz
-#rm train-balanced-sarcasm.gz
+cat train-balanced-sarcasm.parta* > train-balanced-sarcasm.gz
+tar -xvf train-balanced-sarcasm.gz
+rm train-balanced-sarcasm.gz
 
-#cat sarcasm_kaggle.parta* > sarcasm_kaggle.gz
-#tar -xvf sarcasm_kaggle.gz
-#rm sarcasm_kaggle.gz
+cat sarcasm_kaggle.parta* > sarcasm_kaggle.gz
+tar -xvf sarcasm_kaggle.gz
+rm sarcasm_kaggle.gz
 
 cat sentiment.parta* > sentiment.gz
 tar -xvf sentiment.gz
